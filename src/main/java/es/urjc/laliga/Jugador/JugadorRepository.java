1dfbfa85ef9183419bfd4571aeeca791ec7398c9
Carlos Benito <cfernandez@minsait.com>
2017-03-06 16:12:06 +0100

    par of sancion functions

--- src/main/java/es/urjc/laliga/Jugador/JugadorRepository.java
+++ src/main/java/es/urjc/laliga/Jugador/JugadorRepository.java
@@ -1,7 +1,12 @@
 package es.urjc.laliga.Jugador;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.urjc.laliga.Temporada.Temporada;
 
 public interface JugadorRepository extends JpaRepository<Jugador, Long> {
 
+	List<Jugador> findByEquipo_Temporada( Temporada temporada);
 }
